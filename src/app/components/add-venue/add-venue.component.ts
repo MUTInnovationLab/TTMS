4a6fca738347a06db43336ca159c4223a914f691
KwaneleSishi <kwanelesishi050509@gmail.com>
2025-07-02 11:38:32 +0200

    Add Venue modal functionality

--- src/app/components/add-venue/add-venue.component.ts
+++ src/app/components/add-venue/add-venue.component.ts
@@ -1,14 +1,112 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+import { VenueService } from '../../services/Entity Management Services/venue.service';
+import { CommonModule } from '@angular/common'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-venue',
   templateUrl: './add-venue.component.html',
   styleUrls: ['./add-venue.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule
+  ]
 })
-export class AddVenueComponent  implements OnInit {
+export class AddVenueComponent implements OnInit {
+  venueForm: FormGroup;
+  submitted = false;
+  isSubmitting = false;
+  errorMessage: string | null = null;
+  isEditMode = false;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    public modalController: ModalController,
+    private venueService: VenueService
+  ) {
+    this.venueForm = this.fb.group({
+      name: ['', Validators.required],
+      siteName: ['', Validators.required],
+      id: ['', Validators.required],
+      defaultCapacity: ['', [Validators.required, Validators.min(1)]],
+      type: ['', Validators.required],
+      hasProjector: [false],
+      hasWhiteboard: [false],
+      hasSmartboard: [false],
+      hasComputers: [false],
+      computerCount: [{ value: '', disabled: true }, Validators.min(1)]
+    });
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.venueForm.get('hasComputers')?.valueChanges.subscribe(hasComputers => {
+      const computerCount = this.venueForm.get('computerCount');
+      if (hasComputers) {
+        computerCount?.enable();
+      } else {
+        computerCount?.disable();
+        computerCount?.setValue('');
+      }
+    });
+  }
 
+  get f() { return this.venueForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.venueForm.invalid) {
+      return;
+    }
+
+    this.isSubmitting = true;
+    const venueData = {
+      name: this.venueForm.value.name,
+      siteName: this.venueForm.value.siteName,
+      id: this.venueForm.value.id,
+      defaultCapacity: Number(this.venueForm.value.defaultCapacity),
+      type: this.venueForm.value.type,
+      hasProjector: this.venueForm.value.hasProjector,
+      hasWhiteboard: this.venueForm.value.hasWhiteboard,
+      hasSmartboard: this.venueForm.value.hasSmartboard,
+      hasComputers: this.venueForm.value.hasComputers,
+      computerCount: this.venueForm.value.hasComputers ? this.venueForm.value.computerCount : null,
+      autoSchedulable: false,
+      schedulable: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deafLoop: false,
+      area: '',
+      charge: '',
+      department: '',
+      roomId: Math.floor(Math.random() * 1000),
+      staff1Name: '',
+      staff2Name: '',
+      tags: ['1'],
+      telephone: '',
+      website: '',
+      wheelchairAccess: false
+    };
+
+    this.venueService.addVenue(venueData)
+      .then(() => {
+        this.dismissModal();
+      })
+      .catch(error => {
+        this.errorMessage = 'Failed to add venue: ' + error.message;
+        console.error('Firestore error:', error);
+      })
+      .finally(() => {
+        this.isSubmitting = false;
+      });
+  }
+
+  dismissModal() {
+    this.modalController.dismiss();
+  }
 }
--- src/app/services/Entity Management Services/venue.service.ts
+++ src/app/services/Entity Management Services/venue.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { doc, setDoc } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VenueService {
+  constructor(private firestore: AngularFirestore) {}
 
-  constructor() { }
+  async addVenue(venueData: any): Promise<void> {
+    // Get the underlying Firestore instance from AngularFire
+    const db = this.firestore.firestore;
+    
+    // Create a document reference with custom ID
+    const docRef = doc(db, 'venues', venueData.id);
+    
+    // Set the document data
+    return setDoc(docRef, venueData);
+  }
 }
